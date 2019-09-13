e4cc6f8a99a7dc5b7fc0385d73eb297ccb598bf3
rsantanaper <55151826+rsantanaper@users.noreply.github.com>
2019-09-13 12:41:43 -0500

    Add files via upload

---
+++ ProyectoIngEco/src/Formulario/ProyectoFinalIE.java
@@ -0,0 +1,12 @@
+
+package Formulario;
+
+public class ProyectoFinalIE {
+
+    public static void main(String[] args) {
+
+        FrSimulador principal = new FrSimulador();
+        principal.setVisible( true);
+    }
+
+}
